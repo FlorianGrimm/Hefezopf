4468b556e6f92867106fb455b60c3bb895de4150
Florian <grimmuko@googlemail.com>
2016-09-27 23:59:53 +0200

    add ing SP2016

---
+++ HefezopfSharepoint2016WindowsService/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("HefezopfSharepoint2013WindowsService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d7f6ec50-3253-4f22-a8bd-58fad53cba84")]
