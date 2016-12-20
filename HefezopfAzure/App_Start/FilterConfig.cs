1035acaf8cf93f764bfe8c4c336f0911bccb0f9a
f.grimm <f.grimm@TAMAN>
2016-12-20 01:44:52 -0800

    adding HefezopfAzure

---
+++ HefezopfAzure/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HefezopfAzure {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
